fa73b723932b421bd755f3fe518659b8c7fe2afa
Ben Chan <benchan777@gmail.com>
2021-09-03 00:07:18 -0700

    removed unnecessary key

--- public/controllers/routes.js
+++ public/controllers/routes.js
@@ -2,8 +2,6 @@ const axios = require('axios');
 const express = require('express');
 const router = express.Router();
 
-const openWeatherApiKey = '067032376010c13019b8fa3b20785eed'
-
 // Routes
 router.get('/', (req, res) => {
     res.render('home')
