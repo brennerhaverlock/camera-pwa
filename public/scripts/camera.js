4b63812a0d64a7f812d652d49654512a28fbc80a
benchan777 <benchan777@gmail.com>
2021-09-06 16:29:30 -0700

    rearrange order of functions

--- public/scripts/camera.js
+++ public/scripts/camera.js
@@ -8,6 +8,7 @@ const [play, pause, saveImage] = buttons;
 let cameraOn = false;
 let imageCapture;
 let model = undefined;
+let children = [];
 
 // Load coco-ssd (object detection model) 
 cocoSsd.load()
@@ -15,37 +16,6 @@ cocoSsd.load()
   model = loadedModel;
   document.getElementById('information').innerHTML = 'Model has been loaded! You can now start the camera.'
 })
-
-// Start video when play button is clicked
-play.onclick = () => {
-  if (cameraOn == true) {
-      video.play();
-      return;
-  } 
-
-  // Check if camera exists. If it does, start video stream with set constraints
-  if ('mediaDevices' in navigator && navigator.mediaDevices.getUserMedia) {
-    navigator.mediaDevices.enumerateDevices()
-    .then( devices => {
-      const cameras = devices.filter( device => device.kind === 'videoinput');
-      const camera = cameras[cameras.length - 1];
-
-      const videoConstraints = {
-        video: {
-          deviceId: camera.deviceId,
-          facingMode: 'environment',
-          width: 720,
-          height: 1080,
-        }
-      }
-
-      startVideo(videoConstraints);
-    })
-  } else {
-    const errorMessage = document.getElementById('errorMessage');
-    errorMessage.innerHTML += 'getUserMedia() is not supported by this browser.'
-  }
-}
 
 // Function that starts the video stream with input constraints, then displays it on the page
 const startVideo = async (constraints) => {
@@ -70,6 +40,7 @@ const startVideo = async (constraints) => {
   })
 }
 
+// Function that can take a full resolution photo with the camera
 const takePhoto = () => {
   navigator.mediaDevices.enumerateDevices()
   .then( devices => {
@@ -103,6 +74,78 @@ const takePhoto = () => {
   })
 }
 
+// Draws a frame around the subject based on confidence score returned by tensorflow
+const predictWebcam = () => {
+  // Now let's start classifying a frame in the stream.
+  model.detect(video)
+  .then( predictions => {
+    for (let i = 0; i < children.length; i++) {
+      liveVideo.removeChild(children[i]);
+    }
+    children.splice(0);
+
+    // Loop through predictions and draw them to the live view if they
+    // have a high confidence score
+    for (let n = 0; n < predictions.length; n++ ) {
+      if(predictions[n].score > 0.66) {
+        const p = document.createElement('p');
+        p.innerText = predictions[n].class  + ' - with ' 
+          + Math.round(parseFloat(predictions[n].score) * 100) 
+          + '% confidence.';
+        p.style = 'margin-left: ' + predictions[n].bbox[0] + 'px; margin-top: '
+          + (predictions[n].bbox[1] - 10) + 'px; width: ' 
+          + (predictions[n].bbox[2] - 10) + 'px; top: 0; left: 0;';
+
+        const highlighter = document.createElement('div');
+        highlighter.setAttribute('class', 'highlighter');
+        highlighter.style = 'left: ' + predictions[n].bbox[0] + 'px; top: '
+          + predictions[n].bbox[1] + 'px; width: ' 
+          + predictions[n].bbox[2] + 'px; height: '
+          + predictions[n].bbox[3] + 'px;';
+
+        liveVideo.appendChild(highlighter);
+        liveVideo.appendChild(p);
+        children.push(highlighter);
+        children.push(p);
+      }
+    }
+
+    // Call this function again to keep predicting when the browser is ready.
+    window.requestAnimationFrame(predictWebcam);
+  });
+}
+
+// Start video when play button is clicked
+play.onclick = () => {
+  if (cameraOn == true) {
+      video.play();
+      return;
+  } 
+
+  // Check if camera exists. If it does, start video stream with set constraints
+  if ('mediaDevices' in navigator && navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices.enumerateDevices()
+    .then( devices => {
+      const cameras = devices.filter( device => device.kind === 'videoinput');
+      const camera = cameras[cameras.length - 1];
+
+      const videoConstraints = {
+        video: {
+          deviceId: camera.deviceId,
+          facingMode: 'environment',
+          width: 720,
+          height: 1080,
+        }
+      }
+
+      startVideo(videoConstraints);
+    })
+  } else {
+    const errorMessage = document.getElementById('errorMessage');
+    errorMessage.innerHTML += 'getUserMedia() is not supported by this browser.'
+  }
+}
+
 // Pause video stream when pause button is clicked
 pause.onclick = () => {
   video.pause();
@@ -111,47 +154,4 @@ pause.onclick = () => {
 // Save image when save image button is clicked
 saveImage.onclick = () => {
   takePhoto();
-}
-
-var children = [];
-
-function predictWebcam() {
-  // Now let's start classifying a frame in the stream.
-  model.detect(video).then(function (predictions) {
-    // Remove any highlighting we did previous frame.
-    for (let i = 0; i < children.length; i++) {
-      liveVideo.removeChild(children[i]);
-    }
-    children.splice(0);
-    
-    // Now lets loop through predictions and draw them to the live view if
-    // they have a high confidence score.
-    for (let n = 0; n < predictions.length; n++) {
-      // If we are over 66% sure we are sure we classified it right, draw it!
-      if (predictions[n].score > 0.66) {
-        const p = document.createElement('p');
-        p.innerText = predictions[n].class  + ' - with ' 
-            + Math.round(parseFloat(predictions[n].score) * 100) 
-            + '% confidence.';
-        p.style = 'margin-left: ' + predictions[n].bbox[0] + 'px; margin-top: '
-            + (predictions[n].bbox[1] - 10) + 'px; width: ' 
-            + (predictions[n].bbox[2] - 10) + 'px; top: 0; left: 0;';
-
-        const highlighter = document.createElement('div');
-        highlighter.setAttribute('class', 'highlighter');
-        highlighter.style = 'left: ' + predictions[n].bbox[0] + 'px; top: '
-            + predictions[n].bbox[1] + 'px; width: ' 
-            + predictions[n].bbox[2] + 'px; height: '
-            + predictions[n].bbox[3] + 'px;';
-
-        liveVideo.appendChild(highlighter);
-        liveVideo.appendChild(p);
-        children.push(highlighter);
-        children.push(p);
-      }
-    }
-    
-    // Call this function again to keep predicting when the browser is ready.
-    window.requestAnimationFrame(predictWebcam);
-  });
 }
